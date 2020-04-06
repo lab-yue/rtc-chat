b51c44abebcb3a7558df511a75030134867c6229
rainy-me <github@rainy.me>
2020-04-06 23:06:27 +0900

    feat: ssl

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,77 @@
 //import Head from "next/head";
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const Home = () => {
+  const selfRef = useRef<HTMLVideoElement>();
+  const remoteRef = useRef<HTMLVideoElement>();
+
   useEffect(() => {
-    const wsc = new WebSocket('ws://localhost:3000');
+    const wsc = new WebSocket('wss://x.dev:3000');
     wsc.onopen = () => {
       wsc.send('233');
     };
     wsc.onmessage = (e) => console.log(e);
     return () => {};
   }, []);
-  return <>Home</>;
+
+  const createPeerConnection = () => {
+    console.log('createPeerConnection');
+    const peerConnection = new RTCPeerConnection({
+      iceServers: [
+        // Information about ICE servers - Use your own!
+        {
+          urls: 'stun:stun.stunprotocol.org'
+        }
+      ]
+    });
+
+    const handleICECandidate = async () => {
+      console.log('handleICECandidate');
+    };
+    const handleTrack = async () => {
+      console.log('handleTrack');
+    };
+    const handleNegotiationNeeded = async () => {
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+      console.log({
+        sdp: peerConnection.localDescription
+      });
+      console.log('handleNegotiationNeeded');
+    };
+    const handleICEConnectionStateChange = async () => {
+      console.log('handleICEConnectionStateChange');
+    };
+    const handleICEGatheringStateChange = async () => {
+      console.log('handleICEGatheringStateChange');
+    };
+    const handleSignalingStateChange = async () => {
+      console.log('handleSignalingStateChange');
+    };
+
+    peerConnection.onicecandidate = handleICECandidate;
+    peerConnection.ontrack = handleTrack;
+    peerConnection.onnegotiationneeded = handleNegotiationNeeded;
+    peerConnection.oniceconnectionstatechange = handleICEConnectionStateChange;
+    peerConnection.onicegatheringstatechange = handleICEGatheringStateChange;
+    peerConnection.onsignalingstatechange = handleSignalingStateChange;
+  };
+
+  const askMedia = async () => {
+    const selfStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    // createPeerConnection();
+    selfRef.current.srcObject = selfStream;
+    // peerConnection.addStream(selfStream);
+  };
+
+  return (
+    <>
+      Home
+      <video ref={remoteRef} autoPlay />
+      <video ref={selfRef} muted autoPlay />
+      <button onClick={askMedia}>Ask media</button>
+    </>
+  );
 };
 
 export default Home;
--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,22 @@
 import WebSocket from 'ws';
 import express from 'express';
-import http from 'http';
+import https from 'https';
 import bodyParser from 'body-parser';
 import next from 'next';
+import fs from 'fs';
+import path from 'path';
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 const app = express();
-const server = new http.Server(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync(path.resolve(__dirname, '../ssl/x.dev-key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, '../ssl/x.dev.pem'))
+  },
+  app
+);
 //app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,7 +30,8 @@ nextApp.prepare().then(() => {
 
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
+  console.log(req.connection.remoteAddress);
   ws.on('message', (data) => {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
